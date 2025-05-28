954c067987001475be42c81a38ca4eccba9d7734
MohamedElkady0 <101366008+MohamedElkady0@users.noreply.github.com>
2025-05-29 00:59:58 +0300

    pageview edit2

---
+++ lib/core/utils/pageview_string.dart
@@ -0,0 +1,9 @@
+class PageViewString {
+  static const String pageview1 = 'assets/images/chat.png';
+  static const String pageview2 = 'assets/images/newspaper.png';
+  static const String pageview3 = 'assets/images/production.png';
+
+  static const String pageview4 = 'assets/images/store.png';
+  static const String pageview5 = 'assets/images/tv-app.png';
+  static const String pageview6 = 'assets/images/console.png';
+}
--- lib/fetcher/presentation/views/page/page_view.dart
+++ lib/fetcher/presentation/views/page/page_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_chat/core/utils/pageview_string.dart';
+import 'package:my_chat/fetcher/presentation/views/page/widget/background_page.dart';
+import 'package:my_chat/fetcher/presentation/views/page/widget/blur_background.dart';
 
 class PageViewMyChat extends StatefulWidget {
   const PageViewMyChat({super.key});
@@ -7,65 +10,10 @@ class PageViewMyChat extends StatefulWidget {
   State<PageViewMyChat> createState() => _PageViewMyChatState();
 }
 
-class _PageViewMyChatState extends State<PageViewMyChat>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<Color?> colorAnim1;
-  late Animation<Color?> colorAnim2;
-  late Animation<Color?> colorAnim3;
-
-  late Animation<Alignment> topAlignmentAnimation;
-  late Animation<Alignment> bottomAlignmentAnimation;
-
+class _PageViewMyChatState extends State<PageViewMyChat> {
   PageController? _pageController;
-
-  @override
-  void initState() {
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-
-    topAlignmentAnimation = AlignmentTween(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-    ).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
-    );
-
-    bottomAlignmentAnimation = AlignmentTween(
-      begin: Alignment.bottomCenter,
-      end: Alignment.topCenter,
-    ).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
-    );
-
-    colorAnim1 = ColorTween(
-      begin: Colors.deepPurple,
-      end: Colors.lightBlueAccent,
-    ).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.slowMiddle),
-    );
-
-    colorAnim2 = ColorTween(
-      begin: Colors.pinkAccent,
-      end: Colors.greenAccent,
-    ).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.slowMiddle),
-    );
-
-    colorAnim3 = ColorTween(begin: Colors.amber, end: Colors.redAccent).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.slowMiddle),
-    );
-
-    _animationController.repeat(reverse: true);
-
-    super.initState();
-  }
-
   @override
   void dispose() {
-    _animationController.dispose();
     _pageController?.dispose();
     super.dispose();
   }
@@ -73,45 +21,36 @@ class _PageViewMyChatState extends State<PageViewMyChat>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBuilder(
-        animation: _animationController,
-        builder:
-            (context, child) => Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: topAlignmentAnimation.value,
-                  end: bottomAlignmentAnimation.value,
-                  colors: [
-                    colorAnim1.value ?? Colors.transparent,
-                    colorAnim2.value ?? Colors.transparent,
-                    colorAnim3.value ?? Colors.transparent,
-                  ],
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        clipBehavior: Clip.none,
+        controller: _pageController,
+        onPageChanged: (index) {},
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return Stack(
+            children: [
+              BackgroundPage(),
+              BlurBackground(),
+              Center(
+                child: Text(
+                  'Page ${index + 1}',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              child: PageView.builder(
-                controller: _pageController,
-                onPageChanged: (index) {
-                  setState(() {
-                    _animationController.value = index.toDouble();
-
-                    ///edit
-                  });
-                },
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return Center(
-                    child: Text(
-                      'Page ${index + 1}',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  );
-                },
+              Image.asset(
+                PageViewString.pageview1,
+                fit: BoxFit.cover,
+                width: 100,
+                height: 100,
               ),
-            ),
+            ],
+          );
+        },
       ),
     );
   }
