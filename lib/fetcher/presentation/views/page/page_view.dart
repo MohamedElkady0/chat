26237b7fd0dd0ed8e8c69d31eabc1aa834713953
MohamedElkady0 <101366008+MohamedElkady0@users.noreply.github.com>
2025-06-01 15:32:32 +0300

    end pageview

--- lib/fetcher/presentation/views/page/page_view.dart
+++ lib/fetcher/presentation/views/page/page_view.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:my_chat/core/utils/pageview_string.dart';
+import 'package:my_chat/fetcher/presentation/views/page/data/pageview_data.dart';
 import 'package:my_chat/fetcher/presentation/views/page/widget/background_page.dart';
 import 'package:my_chat/fetcher/presentation/views/page/widget/blur_background.dart';
 
@@ -11,46 +13,113 @@ class PageViewMyChat extends StatefulWidget {
 }
 
 class _PageViewMyChatState extends State<PageViewMyChat> {
-  PageController? _pageController;
+  final PageController _controller = PageController(initialPage: 0);
+  int _currentIndex = 0;
+  final _pageIndexNotifier = ValueNotifier<int>(0);
+
+  @override
+  void initState() {
+    super.initState();
+    // Timer.periodic(const Duration(seconds: 6), (timer) {
+    //   if (_currentIndex < pageViewData.length) _currentIndex++;
+
+    //   _controller.animateToPage(
+    //     _currentIndex,
+    //     duration: const Duration(milliseconds: 300),
+    //     curve: Curves.easeIn,
+    //   );
+    // });
+  }
+
   @override
   void dispose() {
-    _pageController?.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView.builder(
-        scrollDirection: Axis.vertical,
-        clipBehavior: Clip.none,
-        controller: _pageController,
-        onPageChanged: (index) {},
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              BackgroundPage(),
-              BlurBackground(),
-              Center(
-                child: Text(
-                  'Page ${index + 1}',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return SafeArea(
+      minimum: EdgeInsets.symmetric(vertical: 8),
+      child: Scaffold(
+        floatingActionButton:
+            _currentIndex < pageViewData.length - 1
+                ? FloatingActionButton(
+                  onPressed: () {
+                    _controller.animateToPage(
+                      _currentIndex + 1,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: const Icon(Icons.arrow_downward),
+                )
+                : Container(),
+        body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          clipBehavior: Clip.none,
+          controller: _controller,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemCount: pageViewData.length,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                BackgroundPage(),
+                BlurBackground(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+
+                  children: [
+                    SizedBox(height: 20),
+                    Image.asset(
+                      pageViewData[index].image!,
+                      fit: BoxFit.fill,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                    ),
+                    Text(
+                      pageViewData[index].title!,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    if (index == pageViewData.length - 1)
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 32,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    SizedBox(height: 20),
+                  ],
                 ),
-              ),
-              Image.asset(
-                PageViewString.pageview1,
-                fit: BoxFit.cover,
-                width: 100,
-                height: 100,
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
